d630c472abddcd040dca3aadccb9f1c04f4d046d
Lisa-Wegner <84021597+Lisa-Wegner@users.noreply.github.com>
2021-07-15 13:10:08 +0200

    Delete Speechalyzer-master directory

--- Speechalyzer-master/src/psk/cmdline/ApplicationSettingsOptions.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 1998 Panos Kougiouris All Rights Reserved
-package psk.cmdline;
-
-public interface ApplicationSettingsOptions
-{
-	final static int optIgnoreUnknown = 1;
-	final static int optDoNotIgnoreUnknown = 0;
-}
